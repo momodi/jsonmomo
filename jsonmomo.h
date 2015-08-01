b51274dabbbeb6bd0dc4883578ac883eb81ae5d8
momodi <info@gaoyunxiang.com>
2015-08-01 17:14:24 +0800

    first version

--- jsonmomo.h
+++ jsonmomo.h
@@ -10,22 +10,29 @@
 #include <memory>
 #include <sstream>
 #include <cctype>
+#include <cstring>
 using namespace std;
 namespace jsonmomo {
     enum Type {
         NUL, INT, DOUBLE, BOOL, STRING, ARRAY, OBJECT
     };
+    class Value;
+    typedef map<string, Value> Object;
+    typedef vector<Value> Array;
     class Value {
         public:
-            Value();
-            Value(int v);
-            Value(int64_t v);
-            Value(double v);
-            Value(bool v);
-            Value(const string &v);
-            Value(const char *v);
+            Value():_type(NUL) {};
+            Value(int v):_type(INT), v_int64(v) {};
+            Value(int64_t v):_type(INT), v_int64(v) {};
+            Value(double v):_type(DOUBLE), v_double(v) {};
+            Value(bool v):_type(BOOL), v_bool(v) {};
+            Value(const string &v):_type(STRING), v_string(v) {};
+            Value(string &&v):_type(STRING), v_string(move(v)) {};
+            Value(const char *v):_type(STRING), v_string(v) {};
             Value(const vector<Value> &v);
+            Value(vector<Value> &&v);
             Value(const map<string, Value> &v);
+            Value(map<string, Value> &&v);
             int asInt() const;
             int64_t asInt64() const;
             double asDouble() const;
@@ -35,13 +42,12 @@ namespace jsonmomo {
             const vector<Value>& asVector() const;
             map<string, Value>& asMap();
             const map<string, Value>& asMap() const;
-
             Type type() const;
 
             Value & operator[](size_t i);
             const Value & operator[](size_t i) const;
-            Value & operator[](const std::string &key);
-            const Value & operator[](const std::string &key) const;
+            Value & operator[](const string &key);
+            const Value & operator[](const string &key) const;
 
             Value& parse(const string &in);
             string write() const;
@@ -55,55 +61,40 @@ namespace jsonmomo {
             double v_double;
             bool v_bool;
             string v_string;
-            vector<Value> v_arr;
-            map<string, Value> v_obj;
+            shared_ptr<vector<Value>> v_arr;
+            shared_ptr<map<string, Value>> v_obj;
             void check_type(const Type &t) const;
             void rec_write(stringstream &O) const;
     };
-    Value::Value() {
-        this->_type = NUL;
-    }
-    Value::Value(int v) {
-        this->_type = INT;
-        this->v_int64 = v;
-    }
-    Value::Value(int64_t v) {
-        this->_type = INT;
-        this->v_int64 = v;
-    }
-    Value::Value(double v) {
-        this->_type = DOUBLE;
-        this->v_double = v;
-    }
-    Value::Value(bool v) {
-        this->_type = BOOL;
-        this->v_bool = v;
-    }
-    Value::Value(const string &v) {
-        this->_type = STRING;
-        this->v_string = v;
-    }
-    Value::Value(const char *v) {
-        this->_type = STRING;
-        this->v_string = v;
-    }
     Value::Value(const vector<Value> &v) {
         this->_type = ARRAY;
-        this->v_arr = v;
+        this->v_arr = shared_ptr<vector<Value>>(new vector<Value>());
+        *(this->v_arr) = v;
+    }
+    Value::Value(vector<Value> &&v) {
+        this->_type = ARRAY;
+        this->v_arr = shared_ptr<vector<Value>>(new vector<Value>());
+        *(this->v_arr) = move(v);
     }
     Value::Value(const map<string, Value> &v) {
         this->_type = OBJECT;
-        this->v_obj = v;
+        this->v_obj = shared_ptr<map<string, Value>>(new map<string, Value>());
+        *(this->v_obj) = v;
+    }
+    Value::Value(map<string, Value> &&v) {
+        this->_type = OBJECT;
+        this->v_obj = shared_ptr<map<string, Value>>(new map<string, Value>());
+        *(this->v_obj) = move(v);
     }
     void Value::check_type(const Type &t) const {
         if (t != this->_type) {
-            throw std::runtime_error("type error");
+            throw runtime_error("type error");
         }
     }
     int Value::asInt() const {
         check_type(INT);
         if ((int)this->v_int64 != this->v_int64) {
-            throw std::runtime_error("Integer is too big than int32");
+            throw runtime_error("Integer is too big than int32");
         }
         return this->v_int64;
     }
@@ -121,88 +112,100 @@ namespace jsonmomo {
     }
     vector<Value>& Value::asVector() {
         check_type(ARRAY);
-        return this->v_arr;
+        return *(this->v_arr);
     }
     const vector<Value>& Value::asVector() const {
         check_type(ARRAY);
-        return this->v_arr;
+        return *(this->v_arr);
     }
     map<string, Value>& Value::asMap() {
         check_type(OBJECT);
-        return this->v_obj;
+        return *(this->v_obj);
     }
     const map<string, Value>& Value::asMap() const {
         check_type(OBJECT);
-        return this->v_obj;
+        return *(this->v_obj);
     }
     Type Value::type() const {
         return this->_type;
     }
     Value & Value::operator[](size_t i) {
         check_type(ARRAY);
-        return this->v_arr[i];
+        return (*(this->v_arr))[i];
     }
     const Value & Value::operator[](size_t i) const {
         check_type(ARRAY);
-        return this->v_arr[i];
+        return (*(this->v_arr))[i];
     }
-    Value & Value::operator[](const std::string &key) {
+    Value & Value::operator[](const string &key) {
         check_type(OBJECT);
-        return this->v_obj[key];
+        return (*(this->v_obj))[key];
     }
-    const Value & Value::operator[](const std::string &key) const {
+    const Value & Value::operator[](const string &key) const {
         check_type(OBJECT);
-        auto v = this->v_obj.find(key);
-        if (v == this->v_obj.end()) {
-            throw std::runtime_error("key:" + key + " missed");
+        auto v = this->v_obj->find(key);
+        if (v == this->v_obj->end()) {
+            throw runtime_error("key:" + key + " missed");
         }
         return v->second;
     }
     Value& Value::parse(const string &in) {
         vector<pair<char, Value>> sta;
+        sta.reserve(1000);
         const char *p = in.c_str();
         for (size_t i = 0; i < in.size(); ++i) {
-            if (isspace(p[i])) {
+            if (isspace(p[i]) || p[i] == '-' || p[i] == ':' || p[i] == ',') {
                 continue;
             }
             if (p[i] == '[') {
-                sta.push_back({'[', Value()});
+                sta.emplace_back('[', Value());
             } else if (p[i] == '{') {
-                sta.push_back({'{', Value()});
+                sta.emplace_back('{', Value());
             } else if (p[i] == ']') {
                 Value now{vector<Value>()};
                 auto &vec = now.asVector();
                 while (not sta.empty() && sta.back().first != '[') {
-                    vec.push_back(sta.back().second);
+                    vec.emplace_back(move(sta.back().second));
                     sta.pop_back();
                 }
                 reverse(vec.begin(), vec.end());
                 if (sta.empty() || sta.back().first != '[') {
-                    throw std::runtime_error("parse error, [] not match");
+                    throw runtime_error("parse error, [] not match");
                 }
                 sta.pop_back();
-                sta.push_back({'a', now});
+                sta.emplace_back('a', move(now));
             } else if (p[i] == '}') {
                 Value now{map<string, Value>()};
                 while (sta.size() >= 2 && sta.back().first != '{') {
                     pair<string, Value> a;
-                    a.second = sta.back().second;
+                    a.second = move(sta.back().second);
                     sta.pop_back();
-                    a.first = sta.back().second.v_string;
+                    if (sta.back().second.type() != STRING) {
+                        throw runtime_error("parse error: some of the keys are not string");
+                    }
+                    a.first = move(sta.back().second.v_string);
                     sta.pop_back();
                     now.asMap().insert(a);
                 }
                 if (sta.empty() || sta.back().first != '{') {
-                    throw std::runtime_error("parse error, {} not match");
+                    throw runtime_error("parse error, {} not match");
                 }
                 sta.pop_back();
-                sta.push_back({'o', now});
+                sta.emplace_back('o', move(now));
             } else if (p[i] == '"') {
                 size_t j = i + 1;
+                string S;
+                S.reserve(100);
                 while (j < in.size() && p[j] != '"') {
-                    j += 1;
+                    if (p[j] == '\\') {
+                        S.push_back(p[j + 1]);
+                        j += 2;
+                    } else {
+                        S.push_back(p[j]);
+                        j += 1;
+                    }
                 }
-                sta.push_back({'v', Value(string(p + i + 1, p + j))});
+                sta.emplace_back('v', Value(S));
                 i = j;
             } else if (isdigit(p[i])) {
                 bool isdouble = false;
@@ -218,24 +221,35 @@ namespace jsonmomo {
                     double v = 0;
                     sscanf(p + i, "%lf", &v);
                     v *= minus_flag;
-                    sta.push_back({'d', Value(v)});
+                    sta.emplace_back('d', Value(v));
                 } else {
                     int64_t v = 0;
                     sscanf(p + i, "%lld", &v);
                     v *= minus_flag;
-                    sta.push_back({'i', Value(v)});
+                    sta.emplace_back('i', Value(v));
                 }
                 while (i < in.size() && p[i] != ',' && p[i] != ']'  && p[i] != '}') {
                     i += 1;
                     continue;
                 }
                 i -= 1;
+            } else if (strncmp("null", p + i, 4) == 0) {
+                sta.emplace_back('n', Value());
+                i += 3;
+            } else if (strncmp("true", p + i, 4) == 0) {
+                sta.emplace_back('b', Value(true));
+                i += 3;
+            } else if (strncmp("false", p + i, 4) == 0) {
+                sta.emplace_back('b', Value(false));
+                i += 4;
+            } else {
+                throw runtime_error(string("parse error:unrecognized character") + (p + i));
             }
         }
         if (sta.size() == 1) {
-            *this = sta.back().second;
+            *this = move(sta.back().second);
         } else {
-            throw std::runtime_error("parse error, type=final");
+            throw runtime_error("parse error, type=final");
         }
         return *this;
     }
@@ -247,13 +261,24 @@ namespace jsonmomo {
         } else if (this->_type == DOUBLE) {
             O << this->v_double;
         } else if (this->_type == BOOL) {
-            O << this->v_bool;
+            if (this->v_bool == true) {
+                O << "true";
+            } else {
+                O << "false";
+            }
         } else if (this->_type == STRING) {
-            O << '"' << this->v_string << '"';
+            O << '"';
+            for (const auto &each: this->v_string) {
+                if (each == '\\' || each == '"') {
+                    O << '\\';
+                }
+                O << each;
+            }
+            O << '"';
         } else if (_type == ARRAY) {
             O << '[';
-            for (auto it = this->v_arr.begin(); it != this->v_arr.end(); ++it) {
-                if (it != this->v_arr.begin()) {
+            for (auto it = this->v_arr->begin(); it != this->v_arr->end(); ++it) {
+                if (it != this->v_arr->begin()) {
                     O << ',';
                 }
                 it->rec_write(O);
@@ -261,8 +286,8 @@ namespace jsonmomo {
             O << ']';
         } else if (_type == OBJECT) {
             O << '{';
-            for (auto it = this->v_obj.begin(); it != this->v_obj.end(); ++it) {
-                if (it != this->v_obj.begin()) {
+            for (auto it = this->v_obj->begin(); it != this->v_obj->end(); ++it) {
+                if (it != this->v_obj->begin()) {
                     O << ',';
                 }
                 O << '"' << it->first << '"' << ':';
